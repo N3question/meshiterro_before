80d403a177f423dcbe11c2384e4a5dcef60d3cdd
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2023-01-21 13:20:30 +0000

    [finish]9章・投稿機能の作成⑵_モデルを準備しよう⑵

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,13 @@ Rails.application.routes.draw do
     # get 'postimages/new' # コントローラ作成時自動追加
     # get 'postimages/index' # コントローラ作成時自動追加
     # get 'postimages/show' # コントローラ作成時自動追加
+  ## resourcesメソット
+    # ルーティングを一括して自動形成してくれる機能
+    # post_imagesコントローラの同じ名前のアクションに紐づく
+      # new, show, index, edit, create, destroy, update
+  ## onlyオプションで生成するルーティングを限定
+    # 今回使用するルーティングだけを[]の中に記述
+      # ＄ rails routes コマンドを実行して確認可能
   resources :post_images, only: [:new, :index, :show]
   
 end
